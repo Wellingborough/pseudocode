db0a4f1fdaad611889dbd7715e9652a12add31a4
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-08-26 16:11:11 +0100

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,9 @@
+//
+// Setup - called on body load...
+//
+function setupEditor() {
+  //
+  // Set up file handler
+  //
+  document.getElementById("load-btn").addEventListener('change', handleFile, false);
+}
