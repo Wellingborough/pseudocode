0892477547c6e14c09b13adc6f061c7e77899947
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-08-26 17:23:16 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -6,5 +6,6 @@ function setupEditor() {
   // Set up file handler
   //
   document.getElementById("load-btn").addEventListener('change', handleFile, false);
+  let cm = new CodeMirror.fromTextArea(document.findElementById("editor"));
 }
 
