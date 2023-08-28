afa2e00e39d16fa1db404bcbd7c2679fdd7a95cf
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-08-28 12:20:43 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -8,7 +8,5 @@ function setupEditor() {
   // Set up file handler
   //
   editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/javascript");
 }
 
