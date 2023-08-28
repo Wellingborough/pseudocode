9bc004740afd508bdce55a81498481be545be95b
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-08-28 16:46:34 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -7,8 +7,40 @@ function setupEditor() {
   //
   // Set up file handler
   //
-  editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/javascript");
+;
+}
+var stashedText = "";
+
+function processInput() {
+  var x = document.getElementById("editor").value;
+
+  if (x != stashedText) {
+    console.log("New Text");
+    console.log(levenshteinDistance(stashedText, x));
+  }
+
+  stashedText = x;
 }
 
+
+const levenshteinDistance = (str1 = '', str2 = '') => {
+   const track = Array(str2.length + 1).fill(null).map(() =>
+   Array(str1.length + 1).fill(null));
+   for (let i = 0; i <= str1.length; i += 1) {
+      track[0][i] = i;
+   }
+   for (let j = 0; j <= str2.length; j += 1) {
+      track[j][0] = j;
+   }
+   for (let j = 1; j <= str2.length; j += 1) {
+      for (let i = 1; i <= str1.length; i += 1) {
+         const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+         track[j][i] = Math.min(
+            track[j][i - 1] + 1, // deletion
+            track[j - 1][i] + 1, // insertion
+            track[j - 1][i - 1] + indicator, // substitution
+         );
+      }
+   }
+   return track[str2.length][str1.length];
+};
